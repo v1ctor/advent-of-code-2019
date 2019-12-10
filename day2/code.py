c68eeb6ae03e648c8c3939e73370c5ebed4c1f5d
Victor Buldakov <victor@buldakov.org>
2019-12-10 10:31:16 +0000

    refactor code

--- day2/code.py
+++ day2/code.py
@@ -1,29 +1,43 @@
 
 
-f = open("input.txt")
 
-s = f.readline()
-#s = "1,0,0,0,99"
+class Computer:
+    def __init__(self, memory, noun, verb):
+        self.memory = memory
+        self.noun = noun
+        self.verb = verb
 
-c = list(map(int, s.split(",")))
+    def execute(self):
+        size = len(self.memory)
+        pc = 0
 
-size = len(c)
-index = 0
+        self.memory[1] = self.noun
+        self.memory[2] = self.verb
 
-c[1] = 12
-c[2] = 2
+        while pc < size:
+            op = self.memory[pc]
+            if op == 99:
+                break
+            addr1 = self.memory[pc + 1]
+            addr2 = self.memory[pc + 2]
+            arg1 = self.memory[addr1]
+            arg2 = self.memory[addr2]
+            addr = self.memory[pc + 3]
+            if op == 1:
+                self.memory[addr] = arg1 + arg2
+            elif op == 2:
+                self.memory[addr] = arg1 * arg2
+            pc += 4
 
-while index < size:
-    op = c[index]
-    if op == 99:
-        break
-    a = c[c[index + 1]]
-    b = c[c[index + 2]]
-    addr = c[index + 3]
-    if op == 1:
-        c[addr] = a + b
-    elif op == 2:
-        c[addr] = a * b
-    index += 4
+        return self.memory[0]
 
-print(c[0])
+def main():
+    f = open("input.txt")
+    s = f.readline()
+    memory = list(map(int, s.split(",")))
+
+    c = Computer(memory, 12, 2)
+    print(c.execute())
+
+if __name__== "__main__":
+    main()
