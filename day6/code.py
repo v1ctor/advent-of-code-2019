d929f6217179d5930e55ca8f0406a4c7b4a0e815
Victor Buldakov <victor@buldakov.org>
2019-12-11 10:17:41 +0000

    solve part 1

--- day6/code.py
+++ day6/code.py
@@ -1,15 +1,17 @@
 import sys
 
-class Orbit:
-    def __init__(self, parent, child):
-        self.parent = parent
-        self.child = child
-
-    def __repr__(self):
-        return "{}->{}".format(self.parent, self.child)
+def compute_orbits(root, path, orbits):
+    if root not in orbits:
+        return path
+    count = 0
+    children = orbits[root]
+    for child in children:
+        count += compute_orbits(child, path + 1, orbits)
+    return count + path
 
 def indirect_orbits(orbits):
-    pass
+    return compute_orbits("COM", 0, orbits)
+
 
 def main():
     filename = "input.txt"
@@ -17,12 +19,17 @@ def main():
     if len(sys.argv) > 1:
         filename = sys.argv[1]
     f = open(filename)
-    orbits = []
+    orbits = {}
     for s in f.readlines():
         pair = s.strip().split(")")
-        orbits.append(Orbit(pair[0], pair[1]))
+        key = pair[0]
+        value = pair[1]
+        if key not in orbits:
+            orbits[key] = []
+        orbits[key].append(value)
 
     print(indirect_orbits(orbits))
+
 
 if __name__== "__main__":
     main()
