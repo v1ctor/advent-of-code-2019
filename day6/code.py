a36353eb1c1781408c61bdee574b401bc2df5315
Victor Buldakov <victor@buldakov.org>
2019-12-11 11:00:10 +0000

    solve a second problem for day 6

--- day6/code.py
+++ day6/code.py
@@ -12,10 +12,28 @@ def compute_orbits(root, path, orbits):
 def indirect_orbits(orbits):
     return compute_orbits("COM", 0, orbits)
 
+def path(root, orbits, destination):
+    if root == destination:
+        return [root]
+    if root not in orbits:
+        return []
+    children = orbits[root]
+    for child in children:
+        p = path(child, orbits, destination)
+        if len(p) != 0:
+            return [root] + p
+    return []
+
+def switch_orbits(orbits):
+    p1 = path("COM", orbits, "YOU")
+    p2 = path("COM", orbits, "SAN")
+    i = 0
+    while len(p1) > i and len(p2) > i and p1[i] == p2[i]:
+        i+=1
+    return len(p1) + len(p2) - 2 * (i + 1) 
 
 def main():
     filename = "input.txt"
-    find_path = False
     if len(sys.argv) > 1:
         filename = sys.argv[1]
     if len(sys.argv) > 2 and sys.argv[2] == "--find-path":
@@ -30,8 +48,10 @@ def main():
             orbits[key] = []
         orbits[key].append(value)
 
-  
-    print(indirect_orbits(orbits))
+    if not find_path:  
+        print(indirect_orbits(orbits))
+    else:
+        print(switch_orbits(orbits))
 
 
 if __name__== "__main__":
