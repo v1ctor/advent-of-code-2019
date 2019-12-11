bbeec39c6656a6852c42d5051e32a2d950d7d46a
Victor Buldakov <victor@buldakov.org>
2019-12-11 23:03:15 +0000

    consider length to sort candidates

--- day10/run.py
+++ day10/run.py
@@ -1,5 +1,6 @@
 import sys
 import heapq
+import math
 
 class Asteroid:
     def __init__(self, x, y, weight):
@@ -9,31 +10,25 @@ class Asteroid:
 
 class Direction:
     direction = []
-    factor = 1
 
 def get_direction(x, y):
 
     limit = min(abs(x), abs(y))
-    f = 1
 
     for factor in range(limit, 1, -1):
         if abs(x) % factor == 0 and abs(y) % factor == 0:
             x /= factor
             y /= factor
-            f = factor
             break
 
 
     if x == 0 and y != 0:
         y /= abs(y)
-        f = abs(y)
     if y == 0 and x != 0:
         x /= abs(x)
-        f = abs(x)
 
     d = Direction()
     d.direction = (x, y)
-    d.factor = f 
     return d
 
 
@@ -41,14 +36,15 @@ def compute_asteroid_map(x, y, rows):
     result = {}
     for i in range(len(rows)):
         for j in range(len(rows[i])):
-            if i == x and j == y:
+            if j == x and i == y:
                 continue
             if rows[i][j] != '#':
                 continue
-            d = get_direction(i - x, j - y)
+            d = get_direction(j - x, i - y)
             if d.direction not in result:
                 result[d.direction] = []
-	    heapq.heappush(result[d.direction], (d.factor, [i, j]))
+            length = math.sqrt(math.pow(j - x, 2) + math.pow(i - y, 2))
+	    heapq.heappush(result[d.direction], (length, [j, i]))
     return result
 
 
@@ -77,7 +73,7 @@ def get_nth_asteroid(x, y, rows, n):
     
     while len(dirs) > 0:
         # x ^ y ^
-    	for i in range(x, len(rows) + 1):
+    	for i in range(x, len(rows[0]) + 1):
 	    for j in range(0, y):
 	    	result = process_sector(i - x, j - y, current, current_round, rounds, dirs, n)
 	    	if result[0] != None:
@@ -85,8 +81,8 @@ def get_nth_asteroid(x, y, rows, n):
 	    	current = result[1]
 
         # x v y ^
-        for i in range(len(rows), x - 1, -1):
-	    for j in range(y, len(rows[0]) + 1):
+        for i in range(len(rows[0]), x - 1, -1):
+	    for j in range(y, len(rows) + 1):
 	        result = process_sector(i - x, j - y, current, current_round, rounds, dirs, n)
 	        if result[0] != None:
 		    return result[0]
@@ -94,7 +90,7 @@ def get_nth_asteroid(x, y, rows, n):
 
         # x v y v
         for i in range(x, -1, -1):
-	    for j in range(len(rows[0]), y - 1, -1):
+	    for j in range(len(rows), y - 1, -1):
 	        result = process_sector(i - x, j - y, current, current_round, rounds, dirs, n)
 	        if result[0] != None:
 		    return result[0]
@@ -117,12 +113,13 @@ def compute_weight(x, y, rows):
     
 def get_target_point(rows):
     result = Asteroid(-1, -1, 0)
-    for i in range(len(rows)):
-        for j in range(len(rows[i])):
-            if rows[i][j] == '#':
-                candidate = compute_weight(i, j, rows)
+    for y in range(len(rows)):
+        for x in range(len(rows[y])):
+            if rows[y][x] == '#':
+                candidate = compute_weight(x, y, rows)
                 if candidate > result.weight:
-                    result = Asteroid(i, j, candidate) 
+                    result = Asteroid(x, y, candidate) 
+
     return result
 
 def main():
@@ -136,6 +133,7 @@ def main():
     rows = f.readlines()
     
     result = get_target_point(rows)
+    print("[{}, {}]".format(result.x, result.y))
     if not destroy:
 	print(result.weight)
     else:
