17a42f1e6592d30966f250015b993f521d518c49
Victor Buldakov <victor@buldakov.org>
2019-12-11 22:43:12 +0000

    draft

--- day10/run.py
+++ day10/run.py
@@ -1,4 +1,5 @@
 import sys
+import heapq
 
 class Asteroid:
     def __init__(self, x, y, weight):
@@ -6,24 +7,34 @@ class Asteroid:
 	self.y = y
 	self.weight = weight
 
+class Direction:
+    direction = []
+    factor = 1
 
 def get_direction(x, y):
 
     limit = min(abs(x), abs(y))
+    f = 1
 
     for factor in range(limit, 1, -1):
         if abs(x) % factor == 0 and abs(y) % factor == 0:
             x /= factor
             y /= factor
+            f = factor
             break
 
 
     if x == 0 and y != 0:
         y /= abs(y)
+        f = abs(y)
     if y == 0 and x != 0:
         x /= abs(x)
+        f = abs(x)
 
-    return (x, y)
+    d = Direction()
+    d.direction = (x, y)
+    d.factor = f 
+    return d
 
 
 def compute_asteroid_map(x, y, rows):
@@ -34,16 +45,72 @@ def compute_asteroid_map(x, y, rows):
                 continue
             if rows[i][j] != '#':
                 continue
-            direction = get_direction(i - x, j - y)
-            if direction not in result:
-                result[direction] = []
-            result[direction].append([i - x, j - y])
-
-#    print("[{}] {}".format(len(result), result))
+            d = get_direction(i - x, j - y)
+            if d.direction not in result:
+                result[d.direction] = []
+	    heapq.heappush(result[d.direction], (d.factor, [i, j]))
     return result
 
+
+def process_sector(x, y, current, current_round, rounds, dirs, n):
+    d = get_direction(x, y)
+    if d.direction in dirs:
+        if d.direction not in rounds:
+	    rounds[d.direction] = 0
+	if rounds[d.direction] < current_round:
+	    rounds[d.direction] = current_round
+	    current += 1
+	    elem = heapq.heappop(dirs[d.direction])
+            if len(dirs[d.direction]) == 0:
+		del dirs[d.direction]
+            print(elem)
+	    if current == n:
+		return (elem, None)
+    return (None, current)
+
+
 def get_nth_asteroid(x, y, rows, n):
-	pass
+    dirs = compute_asteroid_map(x, y, rows)
+    rounds = {}
+    current_round = 1
+    current = 0
+    
+    while len(dirs) > 0:
+        # x ^ y ^
+    	for i in range(x, len(rows) + 1):
+	    for j in range(0, y):
+	    	result = process_sector(i - x, j - y, current, current_round, rounds, dirs, n)
+	    	if result[0] != None:
+		    return result[0]
+	    	current = result[1]
+
+        # x v y ^
+        for i in range(len(rows), x - 1, -1):
+	    for j in range(y, len(rows[0]) + 1):
+	        result = process_sector(i - x, j - y, current, current_round, rounds, dirs, n)
+	        if result[0] != None:
+		    return result[0]
+	        current = result[1]
+
+        # x v y v
+        for i in range(x, -1, -1):
+	    for j in range(len(rows[0]), y - 1, -1):
+	        result = process_sector(i - x, j - y, current, current_round, rounds, dirs, n)
+	        if result[0] != None:
+		    return result[0]
+                current = result[1]
+
+        # x ^ y v
+        for i in range(0, x):
+	    for j in range(y, -1, -1):
+	        result = process_sector(i - x, j - y, current, current_round, rounds, dirs, n)
+	        if result[0] != None:
+		    return result[0]
+	        current = result[1]
+
+        current_round += 1
+    return None
+		         
 
 def compute_weight(x, y, rows):
     return len(compute_asteroid_map(x, y, rows))
@@ -60,7 +127,7 @@ def get_target_point(rows):
 
 def main():
     filename = "input.txt"
-    destory = False
+    destroy = False
     if len(sys.argv) > 1:
         filename = sys.argv[1]
     if len(sys.argv) > 2 and sys.argv[2] == "--destroy":
@@ -69,7 +136,7 @@ def main():
     rows = f.readlines()
     
     result = get_target_point(rows)
-    if not destory:
+    if not destroy:
 	print(result.weight)
     else:
 	print(get_nth_asteroid(result.x, result.y, rows, 200))
