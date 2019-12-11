12b03af544cd29a0e335982af0ed3540038a4249
Victor Buldakov <victor@buldakov.org>
2019-12-11 14:53:17 +0000

    solve puzzel 2 day 8

--- day8/code.py
+++ day8/code.py
@@ -4,16 +4,27 @@ def get_minimum_layer(s, cols, rows):
     chunk_size = cols * rows
     result = 0
     minVal = chunk_size + 1
+    image = list('2' * chunk_size)
     for i in range(0, len(s), chunk_size):
         layer = s[i:i+chunk_size].strip()
         if len(layer) == 0:
             break
+        for j in range(chunk_size):
+            if image[j] != '2':
+                continue
+            else:
+                image[j] = layer[j]
         zero = layer.count('0')
         if zero < minVal:
             minVal = zero
             result = layer.count('1') * layer.count('2')
+    for k in range(0, len(image), cols):
+        print(''.join(image[k:k+cols]))
+
     return result
     
+
+
 
 def main():
     filename = "input.txt"
