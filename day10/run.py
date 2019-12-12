10b53191d06f834802fdeece0a7c21b84f1a7ebc
Victor Buldakov <victor@buldakov.org>
2019-12-12 22:26:46 +0000

    solve day 10 part 2

--- day10/run.py
+++ day10/run.py
@@ -4,10 +4,11 @@ import math
 
 
 def angle(x, y):
-    angle = (math.atan2(y, x) + math.pi / 2) * rad
+    rad = 180 / math.pi
+    angle = math.atan2(-y, x) * rad
     if angle < 0:
         return angle + 360
-    return angle:
+    return angle
 
 
 class Asteroid:
@@ -15,31 +16,6 @@ class Asteroid:
 	self.x = x
 	self.y = y
 	self.weight = weight
-
-
-class Direction:
-    direction = []
-
-
-def get_direction(x, y):
-
-    limit = min(abs(x), abs(y))
-
-    for factor in range(limit, 1, -1):
-        if abs(x) % factor == 0 and abs(y) % factor == 0:
-            x /= factor
-            y /= factor
-            break
-
-
-    if x == 0 and y != 0:
-        y /= abs(y)
-    if y == 0 and x != 0:
-        x /= abs(x)
-
-    d = Direction()
-    d.direction = (x, y)
-    return d
 
 
 def compute_asteroid_map(x, y, rows):
@@ -50,73 +26,13 @@ def compute_asteroid_map(x, y, rows):
                 continue
             if rows[i][j] != '#':
                 continue
-            d = get_direction(j - x, i - y)
-            if d.direction not in result:
-                result[d.direction] = []
+            a = angle(y - i, x - j)
+            if a not in result:
+                result[a] = []
             length = math.sqrt(math.pow(j - x, 2) + math.pow(i - y, 2))
-	    heapq.heappush(result[d.direction], (length, [j, i]))
+	    heapq.heappush(result[a], (length, [j, i]))
     return result
 
-
-def process_sector(x, y, current, current_round, rounds, dirs, n):
-    d = get_direction(x, y)
-    if d.direction in dirs:
-        if d.direction not in rounds:
-	    rounds[d.direction] = 0
-	if rounds[d.direction] < current_round:
-	    rounds[d.direction] = current_round
-	    current += 1
-	    elem = heapq.heappop(dirs[d.direction])
-            if len(dirs[d.direction]) == 0:
-		del dirs[d.direction]
-            print(elem)
-	    if current == n:
-		return (elem, None)
-    return (None, current)
-
-
-def get_nth_asteroid(x, y, rows, n):
-    dirs = compute_asteroid_map(x, y, rows)
-    rounds = {}
-    current_round = 1
-    current = 0
-    
-    while len(dirs) > 0:
-        # x ^ y ^
-    	for i in range(x, len(rows[0]) + 1):
-	    for j in range(0, y):
-	    	result = process_sector(i - x, j - y, current, current_round, rounds, dirs, n)
-	    	if result[0] != None:
-		    return result[0]
-	    	current = result[1]
-
-        # x v y ^
-        for i in range(len(rows[0]), x - 1, -1):
-	    for j in range(y, len(rows) + 1):
-	        result = process_sector(i - x, j - y, current, current_round, rounds, dirs, n)
-	        if result[0] != None:
-		    return result[0]
-	        current = result[1]
-
-        # x v y v
-        for i in range(x, -1, -1):
-	    for j in range(len(rows), y - 1, -1):
-	        result = process_sector(i - x, j - y, current, current_round, rounds, dirs, n)
-	        if result[0] != None:
-		    return result[0]
-                current = result[1]
-
-        # x ^ y v
-        for i in range(0, x):
-	    for j in range(y, -1, -1):
-	        result = process_sector(i - x, j - y, current, current_round, rounds, dirs, n)
-	        if result[0] != None:
-		    return result[0]
-	        current = result[1]
-
-        current_round += 1
-    return None
-		         
 
 def compute_weight(x, y, rows):
     return len(compute_asteroid_map(x, y, rows))
@@ -131,6 +47,32 @@ def get_target_point(rows):
                     result = Asteroid(x, y, candidate) 
 
     return result
+
+def compute_asteroid_queue(x, y, rows):
+    result = []
+    dirs = compute_asteroid_map(x, y, rows)
+    for key, value in dirs.items():
+        heapq.heappush(result, (key, value))
+    return result
+
+
+def get_nth_asteroid(x, y, rows, nth):
+    first = compute_asteroid_queue(x, y, rows)
+    second = []
+    k = 0
+    while len(first) > 0:
+        direction = heapq.heappop(first)
+        asteroid = heapq.heappop(direction[1])
+        print("angle {}, coords {}".format(direction[0], asteroid[1]))
+        k += 1
+        if k == nth:
+            return asteroid[1]
+        if len(direction[1]) > 0:
+            heapq.heappush(second, direction)
+        if len(first) == 0:
+            first, second = second, first
+    return None
+
 
 def main():
     filename = "input.txt"
@@ -147,7 +89,8 @@ def main():
     if not destroy:
 	print(result.weight)
     else:
-	print(get_nth_asteroid(result.x, result.y, rows, 200))
+	coords = get_nth_asteroid(result.x, result.y, rows, 200)
+        print(coords[0] * 100 + coords[1])
 
 
 if __name__== "__main__":
