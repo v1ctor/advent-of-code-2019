d719d8703007e2e8db3c37ac1e3f028bed756a93
Victor Buldakov <victor@buldakov.org>
2019-12-17 21:56:08 +0000

    retab

--- day5/computer.py
+++ day5/computer.py
@@ -6,24 +6,24 @@ class Computer:
         self.pc = 0
 
     def mode(self, mode, pos):
-	for i in range(1, pos):
-	    mode = mode / 10
-	return mode % 10
+        for i in range(1, pos):
+            mode = mode / 10
+        return mode % 10
 
 
     def read(self, pos, mode = -1):
         value = self.memory[self.pc + pos]
         if mode == -1:
-	    return value
-	if self.mode(mode, pos) == 0:
-	    return self.memory[value]
-	else:
-	    return value
+            return value
+        if self.mode(mode, pos) == 0:
+            return self.memory[value]
+        else:
+            return value
 
     # op 1
     def add(self, mode):
-	val1 = self.read(1, mode)
-	val2 = self.read(2, mode)
+        val1 = self.read(1, mode)
+        val2 = self.read(2, mode)
         addr = self.read(3)
         self.memory[addr] = val1 + val2
         self.pc += 4
@@ -38,47 +38,47 @@ class Computer:
 
     # op 5 
     def jump_if_true(self, mode):
-	cond = self.read(1, mode)
-	if cond != 0:
-	    addr = self.read(2, mode)
-	    self.pc = addr
-	else:
-	    self.pc += 3
+        cond = self.read(1, mode)
+        if cond != 0:
+            addr = self.read(2, mode)
+            self.pc = addr
+        else:
+            self.pc += 3
 
     # op 6
     def jump_if_false(self, mode):
-	cond = self.read(1, mode)
-	if cond == 0:
-	    addr = self.read(2, mode)
-	    self.pc = addr
-	else:
-	    self.pc += 3
+        cond = self.read(1, mode)
+        if cond == 0:
+            addr = self.read(2, mode)
+            self.pc = addr
+        else:
+            self.pc += 3
 
     # op 7
     def less_than(self, mode):
-	first = self.read(1, mode)
+        first = self.read(1, mode)
         second = self.read(2, mode)
-	addr = self.read(3)
-	if first < second:
-	    self.memory[addr] = 1
-	else:
-	    self.memory[addr] = 0
-	self.pc += 4
+        addr = self.read(3)
+        if first < second:
+            self.memory[addr] = 1
+        else:
+            self.memory[addr] = 0
+        self.pc += 4
 
     # op 8
     def equals(self, mode):
-	first = self.read(1, mode)
+        first = self.read(1, mode)
         second = self.read(2, mode)
-	addr = self.read(3)
-	if first == second:
-	    self.memory[addr] = 1
+        addr = self.read(3)
+        if first == second:
+            self.memory[addr] = 1
         else:
-	    self.memory[addr] = 0
-	self.pc += 4
+            self.memory[addr] = 0
+        self.pc += 4
 
     # op 3
     def input(self, mode):
-	val = input()
+        val = input()
         addr = self.read(1)
         self.memory[addr] = val
         self.pc += 2
@@ -86,27 +86,27 @@ class Computer:
     # op 4
     def output(self, mode):
         val = self.read(1, mode)
-	print(val)
+        print(val)
         self.pc += 2
 
 
     def execute(self, op, mode): 
             if op == 1:
-		self.add(mode)
+                self.add(mode)
             elif op == 2:
-		self.mult(mode)
+                self.mult(mode)
             elif op == 3:
-		self.input(mode)
+                self.input(mode)
             elif op == 4:
-		self.output(mode)
-	    elif op == 5:
-		self.jump_if_true(mode)
-	    elif op == 6:
-		self.jump_if_false(mode)
-	    elif op == 7:
-		self.less_than(mode)
-	    elif op == 8:
-		self.equals(mode)
+                self.output(mode)
+            elif op == 5:
+                self.jump_if_true(mode)
+            elif op == 6:
+                self.jump_if_false(mode)
+            elif op == 7:
+                self.less_than(mode)
+            elif op == 8:
+                self.equals(mode)
 
 
     def run(self):
@@ -116,8 +116,8 @@ class Computer:
             opcode = self.memory[self.pc]
             op = opcode % 100
             mode = opcode / 100
-	    # print("op: {}".format(opcode))
+            # print("op: {}".format(opcode))
             if op == 99:
-		break
+                break
             self.execute(op, mode)
 
