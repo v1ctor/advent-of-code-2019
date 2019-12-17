6c4a8d583553bc2c4c5c9fb71b5bd3834e0d9ddf
Victor Buldakov <victor@buldakov.org>
2019-12-17 23:41:52 +0000

    solve the first part day 14

--- day14/run.py
+++ day14/run.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 class Ingredient:
     def __init__(self, name, amount):
@@ -9,8 +10,9 @@ class Ingredient:
         return "{} {}".format(self.amount, self.name)
 
 class Reaction:
-    def __init__(self, ingredients, result):
+    def __init__(self, ingredients, amount, result):
         self.ingredients = ingredients
+        self.amount = amount
         self.result = result
 
     def __repr__(self):
@@ -30,10 +32,30 @@ def parse(lines):
         ingredients = []
         for payload in payloads:
             ingredients.append(parse_ingredient(payload.strip()))
-        reactions.append(Reaction(ingredients, result))
+        reactions.append(Reaction(ingredients, result.amount, result))
 
     return reactions
 
+def compute_resources(reactions, leftovers, target, amount, base):
+    if target in leftovers and leftovers[target] > 0:
+        amount -= leftovers[target]
+        leftovers[target] = 0
+    if target == base:
+        return amount
+    reaction = reactions[target]
+    result = 0
+    count = math.ceil(amount / reaction.amount)
+
+    print("target {} amount {} reactions {}".format(target, amount, count))
+    
+    if target not in leftovers:
+        leftovers[target] = 0 
+    leftovers[target] += (reaction.amount * count - amount) 
+    for ingredient in reaction.ingredients:
+        base_ingredient = compute_resources(reactions, leftovers, ingredient.name, count * ingredient.amount, base)
+        print("root: {} name: {} base: {}".format(target, ingredient.name, base_ingredient))
+        result += base_ingredient
+    return result
 
 
 def main():
@@ -48,6 +70,13 @@ def main():
     print(reactions)
 
 
+    leftovers = {}
+    reactions_map = {}
+    for reaction in reactions:
+        reactions_map[reaction.result.name] = reaction
+
+
+    print(compute_resources(reactions_map, leftovers, "FUEL", 1, "ORE"))
 
 if __name__== "__main__":
     main()
