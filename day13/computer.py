d6e22246e21868f37e10f2d3f0b1e0496558cb66
Victor Buldakov <victor@buldakov.org>
2019-12-17 21:50:19 +0000

    replace tbas

--- day13/computer.py
+++ day13/computer.py
@@ -12,35 +12,35 @@ class Computer:
         self.writebuf = []
 
     def mode(self, mode, pos):
-	for i in range(1, pos):
-	    mode = mode / 10
-	return mode % 10
+        for i in range(1, pos):
+            mode = mode / 10
+        return mode % 10
 
 
     def write(self, pos, value, mode):
         addr = self.memory[self.pc + pos]
         value_mode = self.mode(mode, pos)
-	if value_mode == 0:
-	    self.memory[addr] = value
+        if value_mode == 0:
+            self.memory[addr] = value
         elif value_mode == 2:
             self.memory[self.rb + addr] = value
 
     def read(self, pos, mode = -1):
         value = self.memory[self.pc + pos]
         if mode == -1:
-	    return value
+            return value
         value_mode = self.mode(mode, pos)
-	if value_mode == 0:
-	    return self.memory[value]
+        if value_mode == 0:
+            return self.memory[value]
         elif value_mode == 2:
             return self.memory[self.rb + value]
         else:
-	    return value
+            return value
 
     # op 1
     def add(self, mode):
-	val1 = self.read(1, mode)
-	val2 = self.read(2, mode)
+        val1 = self.read(1, mode)
+        val2 = self.read(2, mode)
         addr = self.write(3, val1 + val2, mode)
         self.pc += 4
 
@@ -53,41 +53,41 @@ class Computer:
 
     # op 5 
     def jump_if_true(self, mode):
-	cond = self.read(1, mode)
-	if cond != 0:
-	    addr = self.read(2, mode)
-	    self.pc = addr
-	else:
-	    self.pc += 3
+        cond = self.read(1, mode)
+        if cond != 0:
+            addr = self.read(2, mode)
+            self.pc = addr
+        else:
+            self.pc += 3
 
     # op 6
     def jump_if_false(self, mode):
-	cond = self.read(1, mode)
-	if cond == 0:
-	    addr = self.read(2, mode)
-	    self.pc = addr
-	else:
-	    self.pc += 3
+        cond = self.read(1, mode)
+        if cond == 0:
+            addr = self.read(2, mode)
+            self.pc = addr
+        else:
+            self.pc += 3
 
     # op 7
     def less_than(self, mode):
-	first = self.read(1, mode)
+        first = self.read(1, mode)
         second = self.read(2, mode)
-	if first < second:
-	    self.write(3, 1, mode)
-	else:
-	    self.write(3, 0, mode)
-	self.pc += 4
+        if first < second:
+            self.write(3, 1, mode)
+        else:
+            self.write(3, 0, mode)
+        self.pc += 4
 
     # op 8
     def equals(self, mode):
-	first = self.read(1, mode)
+        first = self.read(1, mode)
         second = self.read(2, mode)
-	if first == second:
-	    self.write(3, 1, mode)
+        if first == second:
+            self.write(3, 1, mode)
         else:
-	    self.write(3, 0, mode)
-	self.pc += 4
+            self.write(3, 0, mode)
+        self.pc += 4
 
     # op 9
     def relative_base(self, mode):
@@ -100,7 +100,7 @@ class Computer:
         if len(self.readbuf) == 0:
             self.icr = 1
         else:
-	    val = self.readbuf[0]
+            val = self.readbuf[0]
             self.readbuf = self.readbuf[1:]
             self.write(1, val, mode)
             self.pc += 2
@@ -124,21 +124,21 @@ class Computer:
 
     def execute(self, op, mode): 
             if op == 1:
-		self.add(mode)
+                self.add(mode)
             elif op == 2:
-		self.mult(mode)
+                self.mult(mode)
             elif op == 3:
-		self.input(mode)
+                self.input(mode)
             elif op == 4:
-		self.output(mode)
-	    elif op == 5:
-		self.jump_if_true(mode)
-	    elif op == 6:
-		self.jump_if_false(mode)
-	    elif op == 7:
-		self.less_than(mode)
-	    elif op == 8:
-		self.equals(mode)
+                self.output(mode)
+            elif op == 5:
+                self.jump_if_true(mode)
+            elif op == 6:
+                self.jump_if_false(mode)
+            elif op == 7:
+                self.less_than(mode)
+            elif op == 8:
+                self.equals(mode)
             elif op == 9:
                 self.relative_base(mode)
 
@@ -153,7 +153,7 @@ class Computer:
             mode = opcode / 100
             # print("op: {}".format(op))
             if op == 99:
-		break
+                break
             self.execute(op, mode)
             if self.icr == 1:
                 break
