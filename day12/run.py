7571cdcab59185afc0364b034be2cab212f54cff
Victor Buldakov <victor@buldakov.org>
2019-12-19 17:49:56 +0000

    replace struct with the raw array

--- day12/run.py
+++ day12/run.py
@@ -1,10 +1,18 @@
 import sys
 import re
-class Moon:
-    def __init__(self, x, y, z):
-        self.pos = [x, y, z]
-        self.vel = [0, 0, 0]
-        self.energy = 0
+import functools 
+
+def gcd(a, b):
+    while b:      
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def lcmm(numbers):
+    return functools.reduce(lcm, numbers)
+
 
 def gravity(x, y):
     if x == y:
@@ -14,50 +22,63 @@ def gravity(x, y):
     if x < y:
         return 1
 
-def run_simulation_iteration(moons, dimension):
-    for i in range(len(moons)):
-        m1 = moons[i]
-        for j in range(i + 1, len(moons)):
-            m2 = moons[j]
-            m1.vel[dimension] += gravity(m1.pos[dimension], m2.pos[dimension])
-            m2.vel[dimension] += gravity(m2.pos[dimension], m1.pos[dimension])
+def run_simulation_iteration(positions, velocities, dimension):
+    for i in range(len(positions) // 3):
+        for j in range(i + 1, len(positions) // 3):
+            velocities[i * 3 + dimension] += gravity(positions[i * 3 + dimension], positions[j * 3 + dimension])
+            velocities[j * 3 + dimension] += gravity(positions[j * 3 + dimension], positions[i * 3 + dimension])
 
-        m1.pos[dimension] += m1.vel[dimension]
+        positions[i * 3 + dimension] += velocities[i * 3 + dimension]
 
 
-def simulate_dimension(moons, dimension, iterations):
+def find_period(positions, velocities, dimension):
+    period = 1
+
+    initial_positions = positions[dimension::3]
+    run_simulation_iteration(positions, velocities, dimension)
+    p = positions[dimension::3]
+    v = velocities[dimension::3]
+    while initial_positions != p or any(x != 0 for x in v):
+        run_simulation_iteration(positions, velocities, dimension)
+        positions = positions[dimension::3]
+        velocities = velocities[dimension::3]
+        period += 1
+    return period
+
+def find_universe_period(positions, velocities):
+    periods = []
+    for i in range(3):
+        periods.append(find_period(positions, velocities, i))
+        print(periods)
+
+    return lcmm(periods)
+
+def simulate_dimension(positions, velocities, dimension, iterations):
     while iterations > 0:
         iterations -= 1
-        run_simulation_iteration(moons, dimension)
+        run_simulation_iteration(positions, velocities, dimension)
 
 
-def simulate(moons, itter):
+def simulate(positions, velocities, itter):
     for i in range(3):
-        simulate_dimension(moons, i, itter)
+        simulate_dimension(positions, velocities, i, itter)
 
-def compute_energy(moons):
+def compute_energy(positions, velocities):
     energy = 0
-    for m in moons:
-        kinetic = 0
-        potential = 0
-        for p in range(3):
-            kinetic += abs(m.pos[p])
-            potential += abs(m.vel[p])
-        energy += kinetic * potential 
-
+    for i in range(len(positions) // 3):
+        energy += sum(map(abs, positions[i * 3:(i + 1) * 3])) * sum(map(abs, velocities[i * 3: (i + 1) * 3]))
     return energy
 
 
 def parse(lines):
-    moons = []
+    positions = []
     for line in lines:
          m = re.match("<x=([-]?[0-9]+), y=([-]?[0-9]+), z=([-]?[0-9]+)", line)
-         x = int(m.group(1))
-         y = int(m.group(2))
-         z = int(m.group(3))
-         moons.append(Moon(x, y, z))
+         positions.append(int(m.group(1)))
+         positions.append(int(m.group(2)))
+         positions.append(int(m.group(3)))
 
-    return moons
+    return positions
 
 
 def main():
@@ -66,12 +87,16 @@ def main():
         filename = sys.argv[1]
     f = open(filename)
     lines = f.readlines()
-    moons = parse(lines)
+    positions = parse(lines)
 
-
+    simulated_positions = list(positions)
+    simulated_velocities = [0] * len(positions)
     itter = int(input("enter number of itterations: "))
-    simulate(moons, itter)
-    print(compute_energy(moons))
+    simulate(simulated_positions, simulated_velocities, itter)
+    print(compute_energy(simulated_positions, simulated_velocities))
+
+    
+    print(find_universe_period(positions, [0] * len(positions)))
 
 
 if __name__== "__main__":
